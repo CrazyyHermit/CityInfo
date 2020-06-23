cf2ac75a2d8ba83418d9daa879abe039d44a84fe
CrazyyEvan <wqqfsm123>
2020-06-23 17:58:39 +0300

    first commit

---
+++ src/flaginfo/Flaginfo.jsx
@@ -0,0 +1,15 @@
+import React, {Component} from "react"
+
+class FlagInfo extends Component {
+
+
+    render() {
+        return(
+            <div>
+                <h1>HELLO</h1>
+            </div>
+        )
+    }
+}
+
+export default FlagInfo
