0e14cf32cb6559d5130448da1ff35d043b32d7d5
CrazyyEvan <wqqfsm123>
2020-07-07 19:26:08 +0300

    Second Commit *Having a hard time commiting stuff, this is mainly for practise.

--- src/flaginfo/Flaginfo.jsx
+++ src/flaginfo/Flaginfo.jsx
@@ -4,14 +4,22 @@ class Flaginfo extends Component {
     constructor() {
         super();
         this.state = {
-            cityInfo: {name:null}
+            cityInfo: "placeholder"
         }
     }
 
     render() {
         return(
             <div id="Flaginfo" hidden="true">
-                <h1>AAAAAAAAAAAAAAAAAAAAA</h1>
+                <h1>This is where the country info will go.</h1>
+                <p>{this.state.cityInfo}</p>
+
+                <p>Altho I'm having a few issues remembering how to import states from other files and changing them, this might take a while.</p>
+                <a href="#" onClick={(e) => {
+                    e.preventDefault()
+                    document.querySelector("#Flaginfo").hidden = "true";
+                    document.querySelector("#container").removeAttribute("hidden")
+                }}>Heck</a>
             </div>
         )
     }
